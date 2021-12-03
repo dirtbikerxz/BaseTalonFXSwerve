0c6d3ed20ebc6c1f60290d9dcd834bb750f1ee1b
agrinmanriv0537 <75352922+agrinmanriv0537@users.noreply.github.com>
2021-12-02 18:29:28 -0600

    new dual action controller axis

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -26,9 +26,12 @@ public class RobotContainer {
   private final Joystick driver = new Joystick(0);
 
   /* Drive Controls */
-  private final int translationAxis = XboxController.Axis.kLeftY.value;
-  private final int strafeAxis = XboxController.Axis.kLeftX.value;
-  private final int rotationAxis = XboxController.Axis.kRightX.value;
+  // private final int translationAxis = XboxController.Axis.kLeftY.value;
+  // private final int strafeAxis = XboxController.Axis.kLeftX.value;
+  // private final int rotationAxis = XboxController.Axis.kRightX.value;
+  private final int translationAxis = 1;
+  private final int strafeAxis = 0;
+  private final int rotationAxis = 2;
 
   /* Driver Buttons */
   private final JoystickButton zeroGyro = new JoystickButton(driver, XboxController.Button.kY.value);
