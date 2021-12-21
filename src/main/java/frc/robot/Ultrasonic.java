3a952e478cd0fad93943bcc21a9358fe2234794c
agrinmanriv0537 <75352922+agrinmanriv0537@users.noreply.github.com>
2021-12-21 00:00:22 -0600

    pretty done

--- src/main/java/frc/robot/Ultrasonic.java
+++ src/main/java/frc/robot/Ultrasonic.java
@@ -5,12 +5,9 @@ import edu.wpi.first.wpilibj.AnalogInput;
 public class Ultrasonic {
     
     private final AnalogInput ultrasonic = new AnalogInput(0);
-    
-    double currentDistanceCentimeters = ultrasonic.getAverageValue() * 0.125;
 
-        if(currentDistanceCentimeters <= 30){
-            System.out.println("Under 30cm");
-        } else {
-            System.out.println(currentDistanceCentimeters + "cm");
-        }
+    public double getDistanceValue(){
+        double currentDistanceCentimeters = ultrasonic.getAverageValue() * 0.125;
+        return currentDistanceCentimeters;
+    }
 }
