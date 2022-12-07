335b3023647d5aa9105b92477d2549f2e3abcdd0
Riley <mackeyr@tmore.com>
2022-12-06 19:27:46 -0600

    Added Absolute Encoder file

---
+++ src/main/java/frc/lib/util/AbsoluteEncoder.java
@@ -0,0 +1,39 @@
+package frc.lib.util;
+
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.wpilibj.AnalogInput;
+import edu.wpi.first.*;
+
+public class AbsoluteEncoder {
+    
+    private final AnalogInput analogInput;
+
+    private Rotation2d rotation2d = new Rotation2d();
+    private Rotation2d home = new Rotation2d();
+
+    public AbsoluteEncoder(int port) {
+
+        analogInput = new AnalogInput(port);
+
+
+    }
+
+    public void homeEncoder() {
+
+        home = rotation2d;
+
+    }
+
+    public Rotation2d getRotation() {
+
+        return home.rotateBy(rotation2d);
+
+    }
+
+    public void getRotationPeriodic() {
+
+        Rotation2d rotation2d = Rotation2d.fromDegrees(360 * (analogInput.getVoltage() / 5.0));
+        
+    }
+
+}
