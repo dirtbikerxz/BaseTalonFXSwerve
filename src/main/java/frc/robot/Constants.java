93e85cf80dfd4e548af4a89acf6902705c7ddf13
Rohit Gondi <dirtbikerxz@gmail.com>
2023-01-08 00:27:29 -0600

    Auto Constants Clarification

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -130,7 +130,7 @@ public final class Constants {
         }
     }
 
-    public static final class AutoConstants {
+    public static final class AutoConstants { //TODO: The below constants are used in the example auto, and must be tuned to specific robot
         public static final double kMaxSpeedMetersPerSecond = 3;
         public static final double kMaxAccelerationMetersPerSecondSquared = 3;
         public static final double kMaxAngularSpeedRadiansPerSecond = Math.PI;
