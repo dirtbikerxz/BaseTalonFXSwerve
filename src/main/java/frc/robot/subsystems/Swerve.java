85bbb48f63e1186a3c4e0f7239e47b0d6cf44345
Cool Erik <ErikSLee@gmail.com>
2023-01-15 09:40:44 -0600

    revert

--- src/main/java/frc/robot/subsystems/Swerve.java
+++ src/main/java/frc/robot/subsystems/Swerve.java
@@ -16,42 +16,18 @@ import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
 import edu.wpi.first.wpilibj.SPI;
-import edu.wpi.first.wpilibj.shuffleboard.BuiltInLayouts;
-import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
-import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardLayout;
-import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Swerve extends SubsystemBase {
     public SwerveDriveOdometry swerveOdometry;
     public SwerveModule[] mSwerveMods;
-    ShuffleboardTab tab; 
-    ShuffleboardLayout flList;
-    ShuffleboardLayout frList;
-    ShuffleboardLayout rlList;
-    ShuffleboardLayout rrList;
-    
   //  public Pigeon2 gyro;
   private final AHRS m_navx = new AHRS(SPI.Port.kMXP, (byte) 200);
 
     public Swerve() {
-        tab = Shuffleboard.getTab("Drivetrain");
-        frList = tab.getLayout("Front Right Module", BuiltInLayouts.kList)
-        .withSize(2, 4)
-        .withPosition(0, 0);
-        flList = tab.getLayout("Front Left Module", BuiltInLayouts.kList)
-        .withSize(2, 4)
-        .withPosition(2, 0);
-        rlList = tab.getLayout("Rear Left Module", BuiltInLayouts.kList)
-        .withSize(2, 4)
-        .withPosition(4, 0);
-        rlList = tab.getLayout("Rear Right Module", BuiltInLayouts.kList)
-        .withSize(2, 4)
-        .withPosition(6, 0);
-        
-
-        tab.add(m_navx);
+        // gyro = new Pigeon2(Constants.Swerve.pigeonID);
+        // gyro.configFactoryDefault();
         zeroGyro();
 
         mSwerveMods = new SwerveModule[] {
@@ -126,40 +102,14 @@ public class Swerve extends SubsystemBase {
         return (Constants.Swerve.invertGyro) ? Rotation2d.fromDegrees(360 - m_navx.getYaw()) : Rotation2d.fromDegrees(m_navx.getYaw());
     }
 
-    public void updateDashboard(){
-
-        flList.add("Cancoder",mSwerveMods[0].getCanCoder().getDegrees());
-        flList.add("Position",mSwerveMods[0].getPosition().angle.getDegrees());
-        flList.add("Velocity",mSwerveMods[0].getState().speedMetersPerSecond);
-        
-        frList.add("Cancoder",mSwerveMods[1].getCanCoder().getDegrees());
-        frList.add("Position",mSwerveMods[1].getPosition().angle.getDegrees());
-        frList.add("Velocity",mSwerveMods[1].getState().speedMetersPerSecond);
-        
-        rlList.add("Cancoder",mSwerveMods[2].getCanCoder().getDegrees());
-        rlList.add("Position",mSwerveMods[2].getPosition().angle.getDegrees());
-        rlList.add("Velocity",mSwerveMods[2].getState().speedMetersPerSecond);
-        
-        rrList.add("Cancoder",mSwerveMods[3].getCanCoder().getDegrees());
-        rrList.add("Position",mSwerveMods[3].getPosition().angle.getDegrees());
-        rrList.add("Velocity",mSwerveMods[3].getState().speedMetersPerSecond);
-
-        // odometryXEntry = tab.add("ODO_X", 0.0)
-        //     .withPosition(8, 0)
-        //     .withSize(1, 1)
-        //     .getEntry();
-        // odometryYEntry = tab.add("ODO_Y", 0.0)
-        //     .withPosition(8, 1)
-        //     .withSize(1, 1)
-        //     .getEntry();
-        // odometryAngleEntry = tab.add("ODO_ANGLE", 0.0)
-        //     .withPosition(8, 2)
-        //     .withSize(1, 1)
-        //     .getEntry();
-    }
     @Override
     public void periodic(){
         swerveOdometry.update(getYaw(), getModulePositions());  
-        updateDashboard();
+
+        for(SwerveModule mod : mSwerveMods){
+            SmartDashboard.putNumber("Mod " + mod.moduleNumber + " Cancoder", mod.getCanCoder().getDegrees());
+            SmartDashboard.putNumber("Mod " + mod.moduleNumber + " Integrated", mod.getPosition().angle.getDegrees());
+            SmartDashboard.putNumber("Mod " + mod.moduleNumber + " Velocity", mod.getState().speedMetersPerSecond);    
+        }
     }
 }
