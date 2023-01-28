b919956d272bc9e3d2f78f58c0deda4d4f879011
Kyle McPherson <84595072+NASA160@users.noreply.github.com>
2023-01-28 11:04:19 -0500

    Fixed the zero gryo command.

--- src/main/java/frc/robot/commands/ZeroGyroCommand.java
+++ src/main/java/frc/robot/commands/ZeroGyroCommand.java
@@ -7,9 +7,13 @@ import frc.robot.subsystems.swerve.SwerveDriveSubsystem;
  * Example of a super-simple command that just runs a method on a subsystem.
  */
 public class ZeroGyroCommand extends RunCommand {
-
+    
     public ZeroGyroCommand(SwerveDriveSubsystem swerveDrive) {
         super(swerveDrive::zeroGyro, swerveDrive);
     }
+    @Override
+    public boolean isFinished() {
+        return true;
+    }                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                  
    
 }
