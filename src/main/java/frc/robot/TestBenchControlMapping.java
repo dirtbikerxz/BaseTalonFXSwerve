7e101e62e171cb8899905e15a65d508b4ea7b16e
Kyle McPherson <84595072+NASA160@users.noreply.github.com>
2023-01-29 15:17:42 -0500

    Pneumatics added.

--- src/main/java/frc/robot/TestBenchControlMapping.java
+++ src/main/java/frc/robot/TestBenchControlMapping.java
@@ -4,6 +4,8 @@ import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.XboxController.Button;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+import frc.robot.commands.HandGrabCommand;
+import frc.robot.commands.HandOffCommand;
 
 /**
  * All of the mapping of controls to commands happens here.
@@ -20,9 +22,11 @@ public class TestBenchControlMapping {
      * 
      * Add additional button/axis mappings here.
      */
-    public static void mapControls(TestBench testBench, XboxController driverController) {
+    public static void mapControls(TestBench testBench, XboxController specialopsController) {
         // TODO map commands here
-        // trigger(specialOpsController, kY, new ExampleCommand(robot));
+        trigger(specialopsController, Button.kY, new HandGrabCommand(testBench.hand, HandGrabCommand.CONE));
+        trigger(specialopsController, Button.kB, new HandGrabCommand(testBench.hand, HandGrabCommand.CUBE));
+        trigger(specialopsController, Button.kRightBumper, new HandOffCommand(testBench.hand));
     }
 
     /**
--- src/main/java/frc/robot/subsystems/HandSubsystem.java
+++ src/main/java/frc/robot/subsystems/HandSubsystem.java
@@ -1,12 +1,25 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.subsystems.HandSubsystem;
+import edu.wpi.first.wpilibj.Compressor;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.PneumaticsModuleType;
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 /**
  * Represents the pneumatics for the hand.
  */
 public class HandSubsystem extends SubsystemBase {
+    public static final String kDefaultAuto = "Default";
+    public static final String kCustomAuto = "My Auto";
+    public String m_autoSelected;
+    public final SendableChooser<String> m_chooser = new SendableChooser<>();
+    public final DoubleSolenoid _Charge = new DoubleSolenoid(5, PneumaticsModuleType.REVPH, 0, 1);
+    public final DoubleSolenoid _Fire = new DoubleSolenoid(5, PneumaticsModuleType.REVPH, 2, 3);
 
-    // TODO fill me out
 
 }
