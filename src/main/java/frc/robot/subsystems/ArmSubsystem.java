3b7f91960678ba6727d208c35b29a9c0b43ba903
KailoskeuZhao <a98165809@gmail.com>
2023-02-02 16:41:51 -0500

    The arm subsystem with some objects framed

--- src/main/java/frc/robot/subsystems/ArmSubsystem.java
+++ src/main/java/frc/robot/subsystems/ArmSubsystem.java
@@ -1,7 +1,37 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.CANSparkMax.IdleMode;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class ArmSubsystem extends SubsystemBase {
+
+    public final double MAXIMUM_POSITION_ROTATING = 42;
+    public final double MININUM_POSITION_ROTATING = 42;
+    public final double MAXIMUM_POSITION_EXTENDING = 42;
+    public final double MININUM_POSITION_EXTENDING = 42;
+
+    public final double TRAVEL_RANGE_ROTATING = 42;
+    public final double TRAVEL_RANGE_EXTEDING = 42;
+
+
+    public CANSparkMax rotatingMotor = new CANSparkMax(1, MotorType.kBrushless);
+    public CANSparkMax extendingMotor = new CANSparkMax(2, MotorType.kBrushless);
+
+    public RelativeEncoder rotatingEncoder = rotatingMotor.getEncoder();
+    public RelativeEncoder extedingEncoder = extendingMotor.getEncoder();
+
+    public DigitalInput limitSwitch0 = new DigitalInput(0);
+    public DigitalInput limitSwitch1 = new DigitalInput(1);
+    public DigitalInput limitSwitch2 = new DigitalInput(2);
+
     
 }
