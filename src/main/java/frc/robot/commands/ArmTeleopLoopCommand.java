67db42cc01d8bb61869a2d5627a879524f035449
KailoskeuZhao <a98165809@gmail.com>
2023-02-02 17:23:44 -0500

    The first prototype of arm command

---
+++ src/main/java/frc/robot/commands/ArmTeleopLoopCommand.java
@@ -0,0 +1,66 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.Subsystem;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.subsystems.ArmSubsystem;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.CANSparkMax.IdleMode;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+import static edu.wpi.first.wpilibj.XboxController.Axis.kLeftX;
+import static edu.wpi.first.wpilibj.XboxController.Axis.kLeftY;
+import static edu.wpi.first.wpilibj.XboxController.Axis.kRightX;
+import static edu.wpi.first.wpilibj.XboxController.Axis.kRightY;
+import static edu.wpi.first.wpilibj.XboxController.Button.kX;
+import static edu.wpi.first.wpilibj.XboxController.Button.kY;
+import static edu.wpi.first.wpilibj.XboxController.Button.kStart;
+
+public class ArmTeleopLoopCommand extends CommandBase {
+
+    private final ArmSubsystem arm;
+    private final XboxController inputController;
+    private boolean done;
+
+    public ArmTeleopLoopCommand(ArmSubsystem armSubsystem, XboxController xboxController){
+
+        this.arm = armSubsystem;
+        this.inputController = xboxController;
+
+    }  
+
+    public void initialize(){
+
+        done = false;
+
+    }
+
+    public void execute(){
+
+        double rotatingMotorPosition = arm.rotatingEncoder.getPosition();
+        double extendingMotorPostition = arm.extendingEncoder.getPosition();
+
+        if (rotatingMotorPosition < arm.MAXIMUM_POSITION_ROTATING || rotatingMotorPosition > arm.MININUM_POSITION_EXTENDING){       
+            arm.rotatingMotor.set(-inputController.getRawAxis(kLeftY.value));
+        }
+
+        if (extendingMotorPostition < arm.MAXIMUM_POSITION_EXTENDING || extendingMotorPostition > arm.MININUM_POSITION_EXTENDING){
+            arm.extendingMotor.set(-inputController.getRawAxis(kRightY.value));
+        }
+
+    }
+
+    public boolean isFinished(){
+
+        return done;
+        
+    }
+}
