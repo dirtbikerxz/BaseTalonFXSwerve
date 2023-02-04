6ad2acadedb799c834c09b7f77488051d52f5816
KailoskeuZhao <a98165809@gmail.com>
2023-02-04 15:05:34 -0500

    Update TestBenchControlMapping.java

--- src/main/java/frc/robot/TestBenchControlMapping.java
+++ src/main/java/frc/robot/TestBenchControlMapping.java
@@ -4,6 +4,7 @@ import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.XboxController.Button;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+import frc.robot.commands.ArmCalabrationCommand;
 import frc.robot.commands.hand.HandGrabCommand;
 import frc.robot.commands.hand.HandReleaseCommand;
 import frc.robot.commands.ArmTurningToAnIndicatedPosition;
