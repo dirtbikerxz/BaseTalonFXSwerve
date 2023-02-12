29e8a04d2bba4851c6e5639be04d39839c275c79
Ed Peters <epeters1111@gmail.com>
2023-02-12 11:43:08 -0500

    Initial implementation of aligning to april tag

--- src/main/java/frc/robot/subsystems/vision/AprilTag.java
+++ src/main/java/frc/robot/subsystems/vision/AprilTag.java
@@ -16,6 +16,20 @@ public class AprilTag {
         this.pose = pose;
     }
 
+    // the vision system uses "z" as the forward/reverse distance to the tag.
+    // the robot will drive in its x dimension to close this distance. this
+    // will hopefully limit confusion when writing the commands.
+    public double getForwardReverseDistance() {
+        return pose.getZ();
+    }
+
+    // the vision system uses "X" as the left/right position of the tag.
+    // the robot will drive in its Y dimension to close this distance. this
+    // will hopefully limit confusion when writing the commands.
+    public double getLeftRightDistance() {
+        return pose.getX();
+    }
+
     public String toString() {
         return String.format("AprilTag(id=%d, imageX=%s, imageY=%s, pose=%s)", id, imageX, imageY, pose);
     }
