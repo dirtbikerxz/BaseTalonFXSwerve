7e49b12604bfd992ae84db93118249bcc383d19b
MORE Robotics <robotics1714@gmail.com>
2023-02-25 14:54:25 -0600

    added disable and enable compressor

--- src/main/java/frc/robot/commands/DisableCompressor.java
+++ src/main/java/frc/robot/commands/DisableCompressor.java
@@ -24,20 +24,28 @@ public class DisableCompressor extends CommandBase {
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
-    pneumaticHub.disableCompressor();
+    if(pneumaticHub.getCompressor()==true){
+      pneumaticHub.disableCompressor();
+    }
+    else{
+      pneumaticHub.enableCompressorDigital();
+    }
   }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
-  public void execute() {}
+  public void execute() {
+  }
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {}
+  public void end(boolean interrupted) {
+
+  }
 
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return false;
+    return true;
   }
 }
