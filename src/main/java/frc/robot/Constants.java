16cb10f9d8574dfbf1596bbf2fc6af5d9eb3b798
Moore <1sammoore714@gmail.com>
2023-03-01 06:29:05 -0600

    Updated Speed Value

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -156,7 +156,7 @@ public final class Constants {
 
         /* Swerve Profiling Values */
         /** Meters per Second */
-        public static final double maxSpeed = 4.5*0.8/* 4.5 */; //TODO: This must be tuned to specific robot
+        public static final double maxSpeed = 4.5/* 4.5 */; //TODO: This must be tuned to specific robot
         /** Radians per Second */
         public static final double maxAngularVelocity = 10.0; //TODO: This must be tuned to specific robot
 
