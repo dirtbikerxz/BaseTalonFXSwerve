96c4edc4e05fefaa6596634b8c4dc9c0f3322ee1
Fritz Geib <fritz@standland.org>
2023-08-28 14:08:58 -0400

    document conversions.java

--- src/main/java/frc/lib/math/Conversions.java
+++ src/main/java/frc/lib/math/Conversions.java
@@ -2,10 +2,22 @@ package frc.lib.math;
 
 public class Conversions {
 
+    /**
+     * Converts rotations of input shaft (eg the motor) to rotations of the output shaft (eg the wheel)
+     * @param inputRotations  The number of rotations of the input shaft
+     * @param gearRatio The gear reduction between the input and output shafts (eg 10:1 would be 10)
+     * @return The number of rotations of the output shaft after the gear reduction
+     */
     public static double getOutputShaftRotations(double inputRotations, double gearRatio) {
         return inputRotations / gearRatio;
     }
 
+    /**
+     * Converts rotations of output shaft (eg the wheel) to rotations of the input shaft (eg the motor)
+     * @param outputRotations The number of rotations of the output shaft
+     * @param gearRatio The gear reduction between the input and output shafts (eg 10:1 would be 10)
+     * @return The number of rotations of the input shaft after the gear reduction
+     */
     public static double getInputShaftRotations(double outputRotations, double gearRatio) {
         return outputRotations * gearRatio;
     }
