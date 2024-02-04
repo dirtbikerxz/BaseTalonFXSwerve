00fb42465710ac2bb4d7e90c5cebb70b9953ed89
Samuel Moore <1sammoore714@gmail.com>
2024-02-04 00:45:03 -0600

    removed open loop delay

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -61,7 +61,7 @@ public final class Constants {
 
         /* These values are used by the drive falcon to ramp in open loop and closed loop driving.
          * We found a small open loop ramp (0.25) helps with tread wear, tipping, etc */
-        public static final double openLoopRamp = 0.25;
+        public static final double openLoopRamp = 0.0;
         public static final double closedLoopRamp = 0.0;
 
         /* Angle Motor PID Values */
