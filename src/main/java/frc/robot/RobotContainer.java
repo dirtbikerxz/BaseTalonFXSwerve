385948c03fe6a49ee90ddd0e3048e5559d25e859
BJ Gerling <107522124+vkskoll@users.noreply.github.com>
2024-02-06 18:51:53 -0600

    updated to match 4011 2024 robot

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -20,6 +20,7 @@ import frc.robot.subsystems.*;
 public class RobotContainer {
     /* Controllers */
     private final Joystick driver = new Joystick(0);
+    //private final XboxController driver = new XboxController(0);
 
     /* Drive Controls */
     private final int translationAxis = XboxController.Axis.kLeftY.value;
