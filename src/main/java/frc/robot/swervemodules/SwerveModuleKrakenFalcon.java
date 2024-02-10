a587eca35bf96e986d3f17aaf708832b120e2af5
Shop <fkalmback@comcast.net>
2024-02-10 15:26:08 -0500

    Created KrakenFalcon Code and using that in Swerve Subsystem

--- src/main/java/frc/robot/swervemodules/SwerveModuleKrakenFalcon.java
+++ src/main/java/frc/robot/swervemodules/SwerveModuleKrakenFalcon.java
@@ -1,4 +1,97 @@
 package frc.robot.swervemodules;
 
-public class SwerveModuleKrakenFalcon {
+import com.ctre.phoenix6.controls.DutyCycleOut;
+import com.ctre.phoenix6.controls.PositionVoltage;
+import com.ctre.phoenix6.controls.VelocityVoltage;
+import com.ctre.phoenix6.hardware.CANcoder;
+import com.ctre.phoenix6.hardware.TalonFX;
+import edu.wpi.first.math.controller.SimpleMotorFeedforward;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.kinematics.SwerveModulePosition;
+import edu.wpi.first.math.kinematics.SwerveModuleState;
+import frc.lib.Conversions;
+import frc.lib.SwerveModuleConstants;
+import frc.lib.doubleNeoConstants;
+import frc.robot.Robot;
+import frc.robot.interfaces.SwerveModule;
+
+public class SwerveModuleKrakenFalcon implements SwerveModule {
+    private final Rotation2d angleOffset;
+
+    private final TalonFX mAngleMotor;
+    private final TalonFX mDriveMotor;
+    private final CANcoder angleEncoder;
+
+    private final SimpleMotorFeedforward driveFeedForward = new SimpleMotorFeedforward(doubleNeoConstants.Swerve.driveKS, doubleNeoConstants.Swerve.driveKV, doubleNeoConstants.Swerve.driveKA);
+
+    /* drive motor control requests */
+    private final DutyCycleOut driveDutyCycle = new DutyCycleOut(0);
+    private final VelocityVoltage driveVelocity = new VelocityVoltage(0);
+
+    /* angle motor control requests */
+    private final PositionVoltage anglePosition = new PositionVoltage(0);
+
+    public SwerveModuleKrakenFalcon(SwerveModuleConstants moduleConstants, int moduleNumber) {
+        this.angleOffset = moduleConstants.angleOffset;
+
+        /* Angle Encoder Config */
+        angleEncoder = new CANcoder(moduleConstants.cancoderID);
+        angleEncoder.getConfigurator().apply(Robot.ctreConfigs.swerveCANcoderConfig);
+
+        /* Angle Motor Config */
+        mAngleMotor = new TalonFX(moduleConstants.angleMotorID);
+        mAngleMotor.getConfigurator().apply(Robot.ctreConfigs.swerveAngleFXConfig);
+        resetToAbsolute();
+
+        /* Drive Motor Config */
+        mDriveMotor = new TalonFX(moduleConstants.driveMotorID);
+        mDriveMotor.getConfigurator().apply(Robot.ctreConfigs.swerveDriveFXConfig);
+        mDriveMotor.getConfigurator().setPosition(0.0);
+    }
+
+    @Override
+    public void setDesiredState(SwerveModuleState desiredState, boolean isOpenLoop) {
+        desiredState = SwerveModuleState.optimize(desiredState, getState().angle);
+        mAngleMotor.setControl(anglePosition.withPosition(desiredState.angle.getRotations()));
+        setSpeed(desiredState, isOpenLoop);
+    }
+
+    @Override
+    public Rotation2d getRotation() {
+        return Rotation2d.fromRotations(angleEncoder.getAbsolutePosition().getValue());
+    }
+
+    @Override
+    public void resetToAbsolute() {
+        double absolutePosition = getRotation().getRotations() - angleOffset.getRotations();
+        mAngleMotor.setPosition(absolutePosition);
+    }
+
+    @Override
+    public SwerveModuleState getState() {
+        return new SwerveModuleState(
+                Conversions.RPSToMPS(mDriveMotor.getVelocity().getValue(), doubleNeoConstants.Swerve.wheelCircumference),
+                Rotation2d.fromRotations(mAngleMotor.getPosition().getValue())
+        );
+    }
+
+    @Override
+    public SwerveModulePosition getPosition() {
+        return new SwerveModulePosition(
+                Conversions.rotationsToMeters(mDriveMotor.getPosition().getValue(), doubleNeoConstants.Swerve.wheelCircumference),
+                Rotation2d.fromRotations(mAngleMotor.getPosition().getValue())
+        );
+    }
+
+    private void setSpeed(SwerveModuleState desiredState, boolean isOpenLoop) {
+        if(isOpenLoop){
+            driveDutyCycle.Output = desiredState.speedMetersPerSecond / doubleNeoConstants.Swerve.maxSpeed;
+            mDriveMotor.setControl(driveDutyCycle);
+        }
+        else {
+            driveVelocity.Velocity = Conversions.MPSToRPS(desiredState.speedMetersPerSecond, doubleNeoConstants.Swerve.wheelCircumference);
+            driveVelocity.FeedForward = driveFeedForward.calculate(desiredState.speedMetersPerSecond);
+            mDriveMotor.setControl(driveVelocity);
+        }
+    }
 }
