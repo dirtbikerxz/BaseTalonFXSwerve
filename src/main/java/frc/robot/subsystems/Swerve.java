39bb2fe8cd763538385cd1b90371d0d803ca5035
joyadomari-chun24 <74032103+joyadomari-chun24@users.noreply.github.com>
2024-02-13 16:27:15 -1000

    Swapped CTRE Pigeon code with Navx code

--- src/main/java/frc/robot/subsystems/Swerve.java
+++ src/main/java/frc/robot/subsystems/Swerve.java
@@ -8,8 +8,8 @@ import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
 import edu.wpi.first.math.kinematics.SwerveDriveOdometry;
 import edu.wpi.first.math.kinematics.SwerveModulePosition;
 
-import com.ctre.phoenix6.configs.Pigeon2Configuration;
 import com.ctre.phoenix6.hardware.Pigeon2;
+import com.kauailabs.navx.frc.AHRS;
 
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
@@ -21,12 +21,11 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 public class Swerve extends SubsystemBase {
     public SwerveDriveOdometry swerveOdometry;
     public SwerveModule[] mSwerveMods;
-    public Pigeon2 gyro;
+    public AHRS navxGyro;
 
     public Swerve() {
-        gyro = new Pigeon2(pigeonID);
-        gyro.getConfigurator().apply(new Pigeon2Configuration());
-        gyro.setYaw(0);
+        navxGyro = new AHRS(SPI.Port.kMXP);
+        navxGyro.zeroYaw();
 
         mSwerveMods = new SwerveModule[] {
             new SwerveModule(0, Mod0.constants),
@@ -105,7 +104,7 @@ public class Swerve extends SubsystemBase {
     }
 
     public Rotation2d getGyroYaw() {
-        return Rotation2d.fromDegrees(gyro.getYaw().getValue());
+        return Rotation2d.fromDegrees(navxGyro.getAngle());
     }
 
     public void resetModulesToAbsolute(){
