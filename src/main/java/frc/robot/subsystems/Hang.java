941665a3c6cfa2d726dec7d1a82101d658df647c
joyadomari-chun24 <74032103+joyadomari-chun24@users.noreply.github.com>
2024-02-15 17:54:40 -1000

    Create Hang.java

---
+++ src/main/java/frc/robot/subsystems/Hang.java
@@ -0,0 +1,31 @@
+package frc.robot.subsystems;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import static frc.robot.Constants.Intake.*;
+
+public class Intake extends SubsystemBase {
+    private final TalonSRX intakeController;
+    private final double intakeSpeed;
+
+    public Intake() {
+        intakeController = new TalonSRX(motorID);
+        intakeSpeed = maxSpeed;
+    }
+
+    @Override
+    public void periodic() {
+        SmartDashboard.putNumber("Intake Speed", intakeSpeed);
+    }
+
+    public void runIntake() {
+        intakeController.set(ControlMode.PercentOutput, intakeSpeed);
+    }
+
+    public void stopIntake() {
+        intakeController.set(ControlMode.PercentOutput, 0);
+    }
+}
